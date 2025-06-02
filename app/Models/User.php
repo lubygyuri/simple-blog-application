a6d7b35ee0949cd28630117c8a25bda61e6af0f5
Lublóváry György <lubigyurma@gmail.com>
2025-06-03 00:06:28 +0200

    Added fillable inputs to User, for backward compatibility.

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,17 @@ class User extends Authenticatable
     use HasFactory, Notifiable, SerializeDates;
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
      * The attributes that should be hidden for serialization.
      *
      * @var list<string>
